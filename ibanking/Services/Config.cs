f9aff8620c3e4cb9a697155d391da12aec025ac5
Samuel J Mancebo <smancebo@cosefi.com>
2018-02-06 11:25:36 -0400

    merged with main

--- ibanking/Services/Config.cs
+++ ibanking/Services/Config.cs
@@ -4,11 +4,11 @@ namespace ibanking.Services
 {
     public static class Config
     {
-        //public static Uri ServiceUrl = new Uri("https://www.cosefi.com/CoopBueno/coopmovil/a_ibanking.asmx");
+        public static Uri ServiceUrl = new Uri("https://www.cosefi.com/CoopBueno/coopmovil/a_ibanking.asmx");
         public static Uri OtherInfo = new Uri("https://www.cosefi.com/CoopBueno/coopmovil/info.html");
-        public static Uri ServiceUrl = new Uri("http://10.172.0.170/ibankingparser/a_ibanking.asmx");
+        //public static Uri ServiceUrl = new Uri("http://10.172.0.170/ibankingparser/a_ibanking.asmx");
         public static string Version = $"v{DependencyService.Get<IVersionGet>().GetVersion()}";
-        public static string DBName = "cosefi.coopmovil.new.db3";
+        public static string DBName = "coopbueno.coopmovil.new.db3";
         public static int GetCurrentAppVersion()
         {
             return Convert.ToInt32(Version.Replace(".", "").Replace("v", ""));
